6228d6410bc19df4eb70bd66ab393fc861f2b2fc
nazarabdullah <nazarabdullah1@gmail.com>
2025-04-10 13:26:00 +0700

    initialize project structure with modularity

---
+++ app/src/main/java/com/kaleci/order_app/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.kaleci.order_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.kaleci.order_app.ui.theme.Order_appTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+            Order_appTheme {
+                // A surface container using the 'background' color from the theme
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = "splash"
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Order_appTheme {
+        Greeting("Android")
+    }
+}
